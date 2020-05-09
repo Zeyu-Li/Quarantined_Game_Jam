79df5add777eff3b07f5b613d49d0e63ce3e32ba
Goldentoaste <a2375658@gmail.com>
2020-05-08 17:31:53 -0700

    2

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -3,12 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour {
+public class move : MonoBehaviour
+{
 
     public Camera camera;
     public CharacterController controller;
 
-    [Header ("ground movement")]
+    [Header("ground movement")]
     public float maxGroundVel = 14f; //units/s
     public float timeToMaxVel = 1f; //time taken to reach maxVel from 0;
 
@@ -16,7 +17,7 @@ public class move : MonoBehaviour {
     public float groundFric = 1;
     //veloctiy += acceleration - friction * velocity
 
-    [Header ("Jumping")]
+    [Header("Jumping")]
 
     public Transform groundCheckLoc;
     public float groundCheckRad;
@@ -27,65 +28,67 @@ public class move : MonoBehaviour {
     public float jumpHeight = 10;
     public float timeToApex = 1; //time taken to reach apex of jump
 
-    [Header ("Air")]
-    public float glideFactor;
-    public float airControl;
-    [Header ("Slope")]
-    public bool isSliding = false;
+
     public Vector3 velocity = Vector3.zero;
 
     // Use this for initialization
-    void Start () {
-        updateNumbers ();
+    void Start()
+    {
+        updateNumbers();
     }
 
     // Update is called once per frame
-    void Update () {
-        groundCheck ();
-        transform.rotation = Quaternion.Euler (0, camera.GetComponent<playerCamera> ().xCurrentRotation, 0);
+    void Update()
+    {
+        groundCheck();
+        transform.rotation = Quaternion.Euler(0, camera.GetComponent<playerCamera>().xCurrentRotation, 0);
         //move before updating velocity!
-        controller.Move (velocity * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
 
         //current input
-        float x = Input.GetAxisRaw ("Horizontal");
-        float z = Input.GetAxisRaw ("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 cInput = (new Vector3 (x, 0, z)).normalized;
+        Vector3 cInput = (new Vector3(x, 0, z)).normalized;
         //rotating the current input to align wiht camera
         cInput = transform.rotation * cInput;
 
         Vector3 acceleration = (cInput * groundAccel);
 
         //the scale is to get stop friction from affecting vertivle movement
-        velocity += (acceleration - groundFric * (Vector3.Scale (velocity, new Vector3 (1, 0, 1)))) * Time.deltaTime;
+        velocity += (acceleration - groundFric * (Vector3.Scale(velocity, new Vector3(1, 0, 1)))) * Time.deltaTime;
 
-        if (Input.GetButtonDown ("Jump") && isGrounded) {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
             velocity.y = jumpVel;
         }
 
-        if (Input.GetButton ("Jump")) {
-            velocity.y += glideFactor * gravity * Time.deltaTime;
-        } else if (!isGrounded) {
+
+        if (!isGrounded)
+        {
             velocity.y += gravity * Time.deltaTime;
         }
-        updateNumbers ();
+        updateNumbers();
 
     }
 
-    void updateNumbers () {
+    void updateNumbers()
+    {
         gravity = -2 * jumpHeight / (timeToApex * timeToApex);
         jumpVel = 2 * jumpHeight / timeToApex;
         groundFric = 5 / (timeToMaxVel); //approximately
         groundAccel = maxGroundVel * groundFric;
     }
-    void groundCheck () {
+    void groundCheck()
+    {
         bool temp = isGrounded;
-        isGrounded = Physics.CheckSphere (groundCheckLoc.position, groundCheckRad, ground);
+        isGrounded = Physics.CheckSphere(groundCheckLoc.position, groundCheckRad, ground);
         if (!temp && isGrounded)
             velocity.y = 0;
     }
 
-    void OnDrawGizmos () {
-        Gizmos.DrawWireSphere (groundCheckLoc.position, groundCheckRad);
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheckLoc.position, groundCheckRad);
     }
 }
