a0f61198f71d782fa4c23ceb2eb0282b73fe59d8
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-05-10 14:42:22 -0600

    updated music

---
+++ Assets/Scripts/dontDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class dontDestroy : MonoBehaviour {
+    // Start is called before the first frame update
+    void Awake() {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        if (SceneManager.GetActiveScene().name == "title") {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
