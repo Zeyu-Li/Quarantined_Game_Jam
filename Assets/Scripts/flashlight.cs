54cded27ed53ee01f35535cdec7a6792b91b5d4a
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-05-09 21:11:48 -0600

    adde title screen and changed floors

---
+++ Assets/Scripts/flashlight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlight : MonoBehaviour
+{
+    public float timeLeft = 180f;
+
+    Light lt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft < 0) {
+            lt.intensity = .2f;
+        }
+    }
+}
