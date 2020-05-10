34954d2de58e77488ce23a11580c6f83917bab75
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-05-10 15:59:22 -0600

    music manager

--- Assets/Scripts/dontDestroy.cs
+++ Assets/Scripts/dontDestroy.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class dontDestroy : MonoBehaviour {
+    private bool a = false;
     // Start is called before the first frame update
     void Awake() {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
-        if (objs.Length > 1)
+        if (objs.Length > 1) {
             Destroy(this.gameObject);
+        }
 
         if (SceneManager.GetActiveScene().name == "title") {
             Destroy(this.gameObject);
